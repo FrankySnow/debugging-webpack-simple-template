b8c668c39e3c319b6f014081ebab5e415afe9e83
Franky Snow <22062145+FrankySnow@users.noreply.github.com>
2018-02-11 23:18:06 +0100

    template webpack-simple
    -scss

---
+++ src/components/breakpointButton.vue
@@ -0,0 +1,31 @@
+<template>  
+	<button class="breakpointButton" @click="triggerBreakpoint">breakpoint</button>
+</template>
+
+<script>
+export default {
+	methods:{
+		triggerBreakpoint(e){
+			console.log(e)
+		}
+	}
+}
+</script>
+
+<style>
+.breakpointButton {
+	background: #41b883;
+	padding: 1em;
+	border: none;
+	color: #35495e;
+	font-weight: bold;
+	cursor: pointer;
+	border: 1px solid #35495e;
+	border-left: 0;
+}
+
+.breakpointButton:hover {
+	background: #35495e;
+	color: white;
+}
+</style>
---
+++ src/components/exceptionButton.vue
@@ -0,0 +1,30 @@
+<template>  
+	<button class="exceptionButton" @click="triggerException">exception</button>
+</template>
+
+<script>
+export default {
+	methods:{
+		triggerException(){
+			myUndefinedFunction()
+		}
+	}
+}
+</script>
+
+<style>
+.exceptionButton {
+	background: #41b883;
+	padding: 1em;
+	border: none;
+	color: #35495e;
+	font-weight: bold;
+	cursor: pointer;
+	border: 1px solid #35495e;
+}
+
+.exceptionButton:hover {
+	background: #35495e;
+	color: white;
+}
+</style>
